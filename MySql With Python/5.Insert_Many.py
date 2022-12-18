79bf167f149173cf49ccec522f574045ff9732de
Harsh <harshpachani@gmail.com>
2022-12-18 20:42:00 +0530

    To insert multiple data into Database

---
+++ MySql With Python/5.Insert_Many.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "Harsh@sql",
+    database = "employee_data"
+)
+
+myCursor = db.cursor()
+# myCursor.execute("USE employee_data")
+sql = "INSERT INTO customers(name, address) VALUES(%s, %s)"
+values = [ #This is List Class.
+    ("Kunj", "Gandhinagar"),
+    ("Toral", "Gandhinagar"),
+    ("Devanshu", "Ahmedabad"),
+    ("Nirav", "Adalaj"),
+]
+# print(type(values))
+for val in values:
+    myCursor.execute(sql, val)
+
+db.commit()
+
+print("{} data added.".format(myCursor.rowcount))
