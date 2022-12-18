cd06cbea914b1328c640d6353e51f846eaf8cf99
Harsh <harshpachani@gmail.com>
2022-12-18 20:41:22 +0530

    To insert data into Database

---
+++ MySql With Python/4.Insert_One.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "Harsh@sql",
+    database = "employee_data"
+)
+
+myCursor = db.cursor()
+
+sql = "INSERT INTO customers(name, address) VALUES(%s, %s)"
+val = ("Harsh", "Ahmedabad")
+myCursor.execute(sql, val)
+
+db.commit() #For Inserting the data to the database.
+
+#To print the number of data added.
+print("{} data added".format(myCursor.rowcount))
