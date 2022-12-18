e628be3c2e0dbc0b2234c0fd09284ebff52b5d05
Harsh <harshpachani@gmail.com>
2022-12-18 20:39:15 +0530

    To create Table in Mysql

---
+++ MySql With Python/3.Create_table.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "Harsh@sql",
+    database = "employee_data"
+)
+
+myCursor = db.cursor()
+sql = """CREATE TABLE customers(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    address VARCHAR(255)
+)
+"""
+myCursor.execute(sql)
+
+print("Table Created Successful !!!")
